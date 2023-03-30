ec9498b39bba638270888b5f61892ff7d14c4d3f
metagamersuniversE <metagamersuniverse@gmail.com>
2023-03-30 06:22:45 +0600

    fff

--- .history/index_20230330061858.js
+++ .history/index_20230330061858.js
@@ -5,7 +5,9 @@ const contractAddress = '0x7F6228DdA3F9ea6B4beAa24181bf95B2F4a29dB8';
 const providerUrl = 'https://arbitrum-mainnet.infura.io/v3/3f689e592e6c45b7bf752c5a97a54ff9';
 const provider = new Web3.providers.HttpProvider(providerUrl);
 const web3 = new Web3(provider);
+
 const contract = new web3.eth.Contract(abi, contractAddress);
+
 const bot = new TelegramBot('6248093145:AAFi5t1Fkbq78LbgkYRxV_8eMYlskaix1as', { polling: true });
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
